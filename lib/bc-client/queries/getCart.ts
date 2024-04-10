61dad90f0c211bfaeb28d23e1dd7b32e521d642e
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-04-10 14:12:38 -0500

    Populate cart info in context

--- lib/bc-client/queries/getCart.ts
+++ lib/bc-client/queries/getCart.ts
@@ -1,12 +1,53 @@
 import { bcGqlFetch } from "../bc-client-gql";
 import { BasicCart, Cart, CartFragment } from "../types/cart";
 
+const getCartQuery = `
+query GetCart($cartId: String!) {
+  site {
+    cart(entityId: $cartId) {
+      ...cartFields
+    }
+  }
+}
+
+${CartFragment}
+`;
+
+type GetCartVars = {
+  cartId: string,
+}
+
+type GetCartResp = {
+  data: {
+    site: {
+      cart: BasicCart & {
+        lineItems: {
+          totalQuantity: number,
+        },
+      },
+    },
+  },
+}
+
 export const getCart: (
   cartId: string
 ) => Promise<Cart> = async (
   cartId
 ) => {
+  const cartResp = await bcGqlFetch<GetCartResp, GetCartVars>(
+    getCartQuery,
+    {
+      cartId
+    }
+  );
+
+  const cart = cartResp.data.site.cart;
+  if (!cart) {
+    throw new Error("Cart not found");
+  }
+
   return {
-    totalQuantity: 0,
+    ...cart,
+    totalQuantity: cart.lineItems.totalQuantity,
   }
 }
--- pages/api/getCustomerSession.ts
+++ pages/api/getCustomerSession.ts
@@ -5,12 +5,31 @@ import { getCart } from '@/lib/bc-client/queries/getCart';
 import getCurrentCustomer from '@/lib/getCurrentCustomer';
 
 type CustomerSessionResp = {
-  
+  cart: Cart | null,
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<CustomerSessionResp>
 ) {
-  res.status(200).json({});
+  if (req.method !== "GET") {
+    res.status(404).json({ cart: null });
+    return;
+  }
+
+  let cart;
+
+  const cartId = getCookie("cartId", { req, res });
+  if (cartId) {
+    try {
+      cart = await getCart(cartId);
+    } catch (err) {
+      cart = null;
+      deleteCookie("cartId", { req, res });
+    }
+  } else {
+    cart = null;
+  }
+
+  res.status(200).json({ cart });
 }
