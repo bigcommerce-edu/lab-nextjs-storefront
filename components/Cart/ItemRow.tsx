4d364b3e74a160f1f9a3c236204bdf145765ef2d
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-04-10 10:47:43 -0500

    Implement cart page

--- components/Cart/ItemRow.tsx
+++ components/Cart/ItemRow.tsx
@@ -4,7 +4,39 @@ import { CartItem } from "@/lib/bc-client/types/cart";
 const CartItemRow = (
   { item, currencyCode }: { item: CartItem, currencyCode: string }
 ) => {
-  return '';
+  const thumbnailSize = 100;
+
+  const currencyFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: currencyCode ?? 'USD',
+  });
+
+  return (
+    <tr className="border-t border-neutral-300">
+      <td className="w-1/2 p-8">
+        {item.imageUrl && (
+          <Image src={item.imageUrl} 
+            alt={item.name}
+            width={thumbnailSize} height={thumbnailSize / 2}
+            className="max-w-2xl max-w-full block" />
+        )}
+        <p className="font-bold">{item.name}</p>
+        {item.sku && (
+          <div className="text-sm">
+            <label className="font-bold">SKU:</label> {item.sku}
+          </div>
+        )}
+        <p>{currencyFormatter.format(item.salePrice.value)}</p>
+      </td>
+      <td className="p-8">
+        <label className="font-bold">Qty:</label>
+        <span> {item.quantity}</span>
+      </td>
+      <td className="p-8 text-right">
+        {currencyFormatter.format(item.extendedSalePrice.value)}
+      </td>
+    </tr>
+  )
 }
 
 export default CartItemRow;
