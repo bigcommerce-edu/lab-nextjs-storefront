96bb99ec6cfd2ef179b825ce0dd5ab83b972b28d
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-04-10 10:47:43 -0500

    Implement cart page

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -10,10 +10,20 @@ import { createCheckoutRedirectUrl } from '@/lib/bc-client/rest/createCheckoutRe
 export const getServerSideProps = (async (context) => {
   const globalProps = await getGlobalServerSideProps(context);
 
+  const { req, res } = context;
+  const cartId = getCookie("cartId", { req, res });
+
+  let cart;
+  try {
+    cart = (cartId) ? await getCartDetails(cartId) : null;
+  } catch (err) {
+    cart = null;
+  }
+
   return {
     props: {
       ... globalProps,
-      cart: { totalQuantity: 0, lineItems: [] },
+      cart,
       checkoutRedirectUrl: '',
     }
   };
@@ -22,9 +32,42 @@ export const getServerSideProps = (async (context) => {
 export default function CartPage(
   { cart, checkoutRedirectUrl }: { cart: CartDetails | null, checkoutRedirectUrl: string }
 ) {
+  if (!cart) {
+    return (
+      <>
+        <PageHeading>Cart</PageHeading>
+        <div className="w-1/2 text-center">There are no items in the cart.</div>
+      </>
+    )
+  }
+
+  const currencyFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: cart?.currencyCode ?? 'USD',
+  });
+
   return (
     <>
-
+      <PageHeading>Cart</PageHeading>
+      <div className="w-full flex justify-center">
+        <table className="w-2/3">
+          <tbody>
+            {cart.lineItems.map(item => (
+              <CartItemRow key={item.entityId} item={item} currencyCode={cart.currencyCode} />
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <th className="px-8 py-4 text-right" colSpan={2}>Subtotal</th>
+              <td className="px-8 py-4 text-right">{currencyFormatter.format(cart.baseAmount.value)}</td>
+            </tr>
+            <tr>
+              <th className="px-8 py-4 text-right" colSpan={2}>Grand Total</th>
+              <td className="px-8 py-4 text-right">{currencyFormatter.format(cart.amount.value)}</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     </>
   )
 }
