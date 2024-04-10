b7ca4c9e0d888629d54af2db592d081fca94b50b
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-04-10 10:09:11 -0500

    Implement basic category page details

--- pages/category/[...catPath].tsx
+++ pages/category/[...catPath].tsx
@@ -13,12 +13,41 @@ import getCurrentCustomer from '@/lib/getCurrentCustomer';
 export const getServerSideProps = (async (context) => {
   const globalProps = await getGlobalServerSideProps(context);
 
+  const pathParam = context.params?.catPath ?? [];
+  const pathSegments = Array.isArray(pathParam) ? pathParam : [pathParam];
+  const path = "/" + pathSegments.join("/");
+
+  const mainImgSize = 500;
+  const thumbnailSize = 500;
+  
+  let category;
+  try {
+    category = await getCategoryWithProducts(
+      path, 
+      mainImgSize, 
+      thumbnailSize,
+      {
+        limit: 12,
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    category = null;
+  }
+
+  if (!category) {
+    return {
+      props: { ... globalProps },
+      notFound: true,
+    }
+  }
+
   return {
     props: {
       ... globalProps,
-      category: { products: [] },
-      mainImgSize: 0,
-      thumbnailSize: 0,
+      category,
+      mainImgSize,
+      thumbnailSize,
     }
   };
 }) satisfies GetServerSideProps;
@@ -28,7 +57,22 @@ export default function CategoryPage(
 ) {
   return (
     <>
+      <PageHeading>{category.name}</PageHeading>
 
+      <div className="w-full max-w-screen-2xl flex justify-center">
+        <div className="px-8 border-x-2 border-neutral-300 xl:w-2/3">
+          {category.defaultImage && (
+            <Image src={category.defaultImage.url} 
+              alt={category.defaultImage.altText ?? ''}
+              width={mainImgSize} height={mainImgSize / 2}
+              className="max-w-full inline-block mr-4 
+                md:w-1/2 md:max-w-3xl md:float-left" />
+          )}
+          {category.description && (
+            <div className="text-lg" dangerouslySetInnerHTML={{__html: category.description}} />
+          )}
+        </div>
+      </div>
     </>
   )
 }
