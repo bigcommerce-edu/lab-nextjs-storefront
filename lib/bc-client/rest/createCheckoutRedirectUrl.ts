2f1b377c257e8871449a143fa1382fa3fadc2ebd
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-04-10 10:50:38 -0500

    Implement redirect to checkout

--- lib/bc-client/rest/createCheckoutRedirectUrl.ts
+++ lib/bc-client/rest/createCheckoutRedirectUrl.ts
@@ -1,9 +1,27 @@
 import { bcRestFetch } from "../bc-client-rest";
 
+type CreateCheckoutRedirectReq = {
+  cartId: string,
+};
+
+type CreateCheckoutRedirectResp = {
+  data: {
+    checkout_url: string,
+  },
+};
+
 export const createCheckoutRedirectUrl: (
   cartId: string
 ) => Promise<string> = async (
   cartId
 ) => {
-  return '';
+  const redirectResp = await bcRestFetch<CreateCheckoutRedirectResp, CreateCheckoutRedirectReq>(
+    `v3/carts/${cartId}/redirect_urls`,
+    "POST",
+    {
+      cartId,
+    }
+  );
+
+  return redirectResp.data?.checkout_url ?? '';
 }
--- pages/cart.tsx
+++ pages/cart.tsx
@@ -24,7 +24,7 @@ export const getServerSideProps = (async (context) => {
     props: {
       ... globalProps,
       cart,
-      checkoutRedirectUrl: '',
+      checkoutRedirectUrl: (cartId) ? await createCheckoutRedirectUrl(cartId) : '',
     }
   };
 }) satisfies GetServerSideProps;
@@ -64,6 +64,19 @@ export default function CartPage(
             <tr>
               <th className="px-8 py-4 text-right" colSpan={2}>Grand Total</th>
               <td className="px-8 py-4 text-right">{currencyFormatter.format(cart.amount.value)}</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td className="p-8 text-right" colSpan={2}>
+                {checkoutRedirectUrl && (
+                  <a className="p-2 rounded-md text-lg px-4 font-normal cursor-pointer
+                    bg-neutral-700 text-white hover:bg-neutral-500 disabled:bg-neutral-500
+                    hover:no-underline"
+                    href={checkoutRedirectUrl}>
+                    Proceed to Checkout
+                  </a>
+                )}
+              </td>
             </tr>
           </tfoot>
         </table>
