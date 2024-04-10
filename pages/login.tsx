53fa9baed40f13b1dc0887ff6869eacb8026ec3a
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-04-10 14:15:11 -0500

    Implement login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -14,9 +14,78 @@ export const getServerSideProps = (async (context) => {
 }) satisfies GetServerSideProps;
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const { setLoggedIn } = useCustomerSession();
+  const router = useRouter();
+
+  const submitLogin = async () => {
+    setLoading(true);
+    setErrorMsg(null);
+
+    const loginResp = await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password
+      }),
+    }).then(res => res.json());
+
+    setLoading(false);
+    
+    if (loginResp.loggedIn) {
+      setLoggedIn(true);
+      router.push("/");
+    } else {
+      setErrorMsg(loginResp.error ?? "An unexpected error occurred");
+    }
+  }
+
   return (
     <>
+      <PageHeading>Log In</PageHeading>
+      <div className="w-1/3">
+        <form onSubmit={(e) => {e.preventDefault(); submitLogin()}}
+          className="">
+          {errorMsg && (
+            <div className="bg-rose-400 p-2 font-bold text-sm text-center">
+              {errorMsg}
+            </div>
+          )}
 
+          <div className="my-8 p-4">
+            <label className="font-bold text-lg block">Email</label>
+            <input className="w-full border border-neutral-300 p-2
+              hover:border-neutral-700"
+              type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div className="my-8 p-4">
+            <label className="font-bold text-lg block">Password</label>
+            <input className="w-full border border-neutral-300 p-2
+              hover:border-neutral-700"
+              type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <div className="my-8 p-4 text-center">
+          <button disabled={loading} className="p-2 rounded-md text-lg w-44
+            bg-neutral-700 text-white hover:bg-neutral-500 disabled:bg-neutral-500">
+            {loading ? (
+              <span>...</span>
+            ) : (
+              <span>
+                Log In
+              </span>
+            )}
+          </button>
+          </div>
+        </form>
+      </div>
     </>
   )
 }
