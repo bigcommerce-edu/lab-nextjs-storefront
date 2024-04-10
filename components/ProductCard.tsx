edb0f29ac18b865731546546d99d4937c01d9c09
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-04-10 10:12:05 -0500

    Implement product listing

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -5,7 +5,26 @@ import { CategoryProduct } from "@/lib/bc-client/types/catalog";
 const ProductCard = (
   { product, thumbnailSize }: { product: CategoryProduct, thumbnailSize: number }
 ) => {
-  return '';
+  const currencyFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: product.prices.price.currencyCode,
+  });
+  
+  return (
+    <Link href={`/product${product.path}`}>
+      {product.defaultImage && (
+        <Image src={product.defaultImage.url} 
+          alt={product.defaultImage.altText ?? ''}
+          width={thumbnailSize} height={thumbnailSize / 2}
+          className="max-w-full inline-block" />
+      )}
+      <h2 className="text-lg">{product.name}</h2>
+      <p className="font-normal text-sm">{product.sku}</p>
+      <p className="font-normal my-2">
+        {currencyFormatter.format(product.prices.price.value)}
+      </p>
+    </Link>
+  );
 }
 
 export default ProductCard;
