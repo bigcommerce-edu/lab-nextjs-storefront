dca5d047c2761bfca693beb5556fa7b4c15a1a28
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-04-10 10:59:46 -0500

    Implement login mutation and route

--- lib/bc-client/mutations/login.ts
+++ lib/bc-client/mutations/login.ts
@@ -1,10 +1,52 @@
 import { bcGqlFetch } from "../bc-client-gql";
 import { Customer } from "../types/customer";
 
+const loginQuery = `
+mutation Login(
+  $email: String!,
+  $password: String!
+) {
+  login(
+    email: $email,
+    password: $password
+  ) {
+    customer {
+      entityId
+    }
+  }
+}
+`;
+
+type LoginVars = {
+  email: string,
+  password: string,
+}
+
+type LoginResp = {
+  data: {
+    login: {
+      customer: Customer,
+    },
+  },
+}
+
 export const login: (
   email: string, password: string
 ) => Promise<Customer> = async (
   email, password
 ) => {
-  return {};
+  const customerResp = await bcGqlFetch<LoginResp, LoginVars>(
+    loginQuery,
+    {
+      email,
+      password,
+    }
+  );
+
+  const customer = customerResp.data.login.customer;
+  if (!customer) {
+    throw new Error("Customer login failed");
+  }
+
+  return customer;
 }
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -3,9 +3,42 @@ import { setCookie } from "cookies-next";
 import { login } from '@/lib/bc-client/mutations/login';
 import * as jwt from 'jsonwebtoken';
 
+type LoginReq = {
+  email: string,
+  password: string,
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<{ loggedIn: boolean, error?: string }>
 ) {
-  res.status(200).json({ loggedIn: false });
+  if (req.method !== "POST") {
+    res.status(404).json({ loggedIn: false });
+    return;
+  }
+
+  const { JWT_SECRET } = process.env;
+  if (!JWT_SECRET) {
+    res.status(401).json({ loggedIn: false, error: "Could not perform login" });
+    return;
+  }
+
+  const reqData = req.body satisfies LoginReq;
+  const { email, password } = reqData;
+
+  try {
+    const customer = await login(email, password);
+
+    const customerToken = jwt.sign({
+      sub: customer.entityId,
+    }, JWT_SECRET);
+
+    setCookie("customer", customerToken, { req, res, httpOnly: true, secure: true });
+
+    res.status(200).json({ loggedIn: true });
+  } catch (err)  {
+    const error = (err instanceof Error) ? err.message : String(err);
+
+    res.status(401).json({ loggedIn: false, error });
+  }
 }
