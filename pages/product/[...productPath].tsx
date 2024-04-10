844c2c42da06fc49db4646d0f1e1df91d66499ae
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-04-10 10:27:32 -0500

    Implement product page

--- pages/product/[...productPath].tsx
+++ pages/product/[...productPath].tsx
@@ -10,11 +10,32 @@ import getCurrentCustomer from '@/lib/getCurrentCustomer';
 export const getServerSideProps = (async (context) => {
   const globalProps = await getGlobalServerSideProps(context);
 
+  const pathParam = context.params?.productPath ?? [];
+  const pathSegments = Array.isArray(pathParam) ? pathParam : [pathParam];
+  const path = "/" + pathSegments.join("/");
+
+  const imgSize = 900;
+
+  let product;
+  try {
+    product = await getProduct(path, imgSize);
+  } catch (err) {
+    console.log(err);
+    product = null;
+  }
+
+  if (!product) {
+    return {
+      props: {... globalProps},
+      notFound: true,
+    }
+  }
+
   return {
     props: {
       ... globalProps,
-      product: {},
-      imgSize: 0,
+      product,
+      imgSize,
     }
   };
 }) satisfies GetServerSideProps;
@@ -22,9 +43,35 @@ export const getServerSideProps = (async (context) => {
 export default function ProductPage(
   { product, imgSize }: { product: Product, imgSize: number }
 ) {
+  const currencyFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: product.prices.price.currencyCode,
+  });
+
   return (
     <>
-
+      <PageHeading>{product.name}</PageHeading>
+      <div className="w-full max-w-screen-2xl flex flex-wrap justify-center">
+        <div className="w-1/2">
+          {product.defaultImage && (
+            <Image src={product.defaultImage.url} 
+              alt={product.defaultImage.altText ?? ''}
+              width={imgSize} height={imgSize / 2}
+              className="" />
+          )}
+        </div>
+        <div className="w-1/2 p-4">
+          <p className="my-2"><label className="font-bold">SKU:</label> {product.sku}</p>
+          {product.description && (
+            <div dangerouslySetInnerHTML={{__html: product.description}}
+              className="my-2" />
+          )}
+          <div className="text-lg my-4">
+            <label className="font-bold">Price:</label>
+            <span> {currencyFormatter.format(product.prices.price.value)}</span>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
