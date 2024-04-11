cb33819ab649a1cfdc60cedf6c0317db9d9a5bfa
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-04-11 11:14:14 -0500

    Implement Add to Cart mutations and route

--- lib/bc-client/types/cart.ts
+++ lib/bc-client/types/cart.ts
@@ -1,5 +1,9 @@
 export type BasicCart = {
-  
+  entityId: string,
+  currencyCode: string,
+  amount: {
+    value: number,
+  },
 }
 
 export type Cart = BasicCart & {
@@ -7,7 +11,16 @@ export type Cart = BasicCart & {
 }
 
 export const CartFragment = `
-
+fragment cartFields on Cart {
+  entityId
+  currencyCode
+  amount {
+    value
+  }
+  lineItems {
+    totalQuantity
+  }
+}
 `
 
 export type CartItem = {
