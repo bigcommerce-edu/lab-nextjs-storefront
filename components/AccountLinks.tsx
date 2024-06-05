bdc864b98dbbf3991cb3ecc0309dd13b445a310e
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-06-05 12:03:06 -0500

    Add account links in header

--- components/AccountLinks.tsx
+++ components/AccountLinks.tsx
@@ -4,9 +4,36 @@ import { useCustomerSession } from "@/context/customerSession";
 import { useRouter } from "next/router";
 
 const AccountLinks = () => {
+  const [loading, setLoading] = useState(false);
+
+  const { loggedIn } = useCustomerSession();
+  const router = useRouter();
+
+  const logOut = async () => {
+    setLoading(true);
+    await fetch("/api/logout", { method: "POST" });
+    setLoading(false);
+    router.reload();
+  }
+
   return (
     <>
-
+      {loggedIn ? (
+        <button disabled={loading} className="mx-4 font-bold hover:underline"
+          onClick={logOut}>
+          {loading ? (
+            <span>...</span>
+          ) : (
+            <span>Log out</span>
+          )}
+        </button>
+      ) : (
+        <>
+          <Link href="/register" className="mx-4">Register</Link>
+          |
+          <Link href="/login" className="mx-4">Log in</Link>
+        </>
+      )}
     </>
   )
 }
