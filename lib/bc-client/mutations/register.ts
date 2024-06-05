9b0fc02c18b0bb549f15e4ec7faaf97d158cdcc0
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-06-05 11:58:57 -0500

    Implement register mutation and route

--- lib/bc-client/mutations/register.ts
+++ lib/bc-client/mutations/register.ts
@@ -1,6 +1,65 @@
 import { bcGqlFetch } from "../bc-client-gql";
 import { Customer } from "../types/customer";
 
+const registerQuery = `
+mutation Register(
+  $email: String!,
+  $firstName: String!,
+  $lastName: String!,
+  $password: String!
+) {
+  customer {
+    registerCustomer(
+      input: {
+        email: $email,
+        firstName: $firstName,
+        lastName: $lastName,
+        password: $password
+      }
+    ) {
+      customer {
+        entityId
+      }
+      errors {
+        ... on EmailAlreadyInUseError {
+          message
+        }
+        ... on AccountCreationDisabledError {
+          message
+        }
+        ... on CustomerRegistrationError {
+          message
+        }
+        ... on ValidationError {
+          message
+        }
+      }
+    }
+  }
+}
+`;
+
+type RegisterVars = {
+  email: string,
+  firstName: string,
+  lastName: string,
+  password: string,
+}
+
+type RegisterResp = {
+  data: {
+    customer: {
+      registerCustomer: {
+        customer: Customer,
+        errors?: {
+          message: string,
+        }[],
+      },
+    },
+  },
+}
+
+
 export const register: (
   email: string, 
   firstName: string,
@@ -9,5 +68,25 @@ export const register: (
 ) => Promise<Customer> = async (
   email, firstName, lastName, password
 ) => {
-  return {};
+  const customerResp = await bcGqlFetch<RegisterResp, RegisterVars>(
+    registerQuery,
+    {
+      email,
+      firstName,
+      lastName,
+      password,
+    }
+  );
+
+  const customer = customerResp.data.customer.registerCustomer.customer;
+  const errors = customerResp.data.customer.registerCustomer.errors;
+  if (!customer) {
+    let errorMsg = "Customer registration failed";
+    if (errors) {
+      errorMsg = errors.map(error => error.message).join("; ")
+    }
+    throw new Error(errorMsg);
+  }
+
+  return customer;
 }
--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,9 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { register } from '@/lib/bc-client/mutations/register';
 
+type RegisterReq = {
+  email: string,
+  firstName: string,
+  lastName: string,
+  password: string,
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<{ success: boolean, error?: string }>
 ) {
-  res.status(200).json({ success: false });
+  if (req.method !== "POST") {
+    res.status(404).json({ success: false });
+    return;
+  }
+
+  const reqData = req.body satisfies RegisterReq;
+  const { email, firstName, lastName, password } = reqData;
+
+  try {
+    const customer = await register(email, firstName, lastName, password);
+
+    res.status(200).json({ success: true });
+  } catch (err)  {
+    const error = (err instanceof Error) ? err.message : String(err);
+
+    res.status(401).json({ success: false, error });
+  }
 }
