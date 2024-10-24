a313838354a50761855604ffd02dce39958f7661
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-10-24 17:54:55 -0500

    Implement category query

--- lib/bc-client/types/catalog.ts
+++ lib/bc-client/types/catalog.ts
@@ -1,9 +1,27 @@
 export type CategoryProduct = {
-  
+  sku: string,
+  name: string,
+  path: string,
+  prices: {
+    price: {
+      value: number,
+      currencyCode: string,
+    }
+  }
+  defaultImage?: {
+    url: string,
+    altText?: string,
+  }
 }
 
 export type BasicCategory = {
-  
+  name: string,
+  path: string,
+  description?: string,
+  defaultImage?: {
+    url: string,
+    altText?: string,
+  }
 }
 
 export type Category = BasicCategory & {
