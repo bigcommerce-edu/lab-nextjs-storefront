7dab47de43d300fb25fc2de3794873c2d889a71a
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-10-24 17:46:15 -0500

    Implement login mutation and route

--- lib/bc-client/mutations/login.ts
+++ lib/bc-client/mutations/login.ts
@@ -1,10 +1,64 @@
 import { bcGqlFetch } from "../bc-client-gql";
 import { Customer } from "../types/customer";
 
+const loginQuery = `
+mutation Login(
+  $email: String!,
+  $password: String!
+) {
+  login(
+    email: $email,
+    password: $password
+  ) {
+    customer {
+      entityId
+    }
+    customerAccessToken {
+        value
+    }
+  }
+}
+`;
+
+type LoginVars = {
+  email: string,
+  password: string,
+}
+
+type LoginResp = {
+  data: {
+    login: {
+      customer: {
+        entityId: number,
+      },
+      customerAccessToken: {
+        value: string,
+      },
+    },
+  },
+}
+
 export const login: (
   email: string, password: string
 ) => Promise<Customer> = async (
   email, password
 ) => {
-  return {};
+  const customerResp = await bcGqlFetch<LoginResp, LoginVars>(
+    loginQuery,
+    {
+      email,
+      password,
+    }
+  );
+
+  const customer = customerResp.data.login.customer;
+  const token = customerResp.data.login.customerAccessToken.value;
+  if (!customer || !token) {
+    throw new Error("Customer login failed");
+  }
+
+  return {
+    entityId: customer.entityId,
+    token,
+  };
 }
--- lib/bc-client/types/customer.ts
+++ lib/bc-client/types/customer.ts
@@ -1,3 +1,4 @@
 export type Customer = {
   entityId: number,
+  token?: string,
 }
