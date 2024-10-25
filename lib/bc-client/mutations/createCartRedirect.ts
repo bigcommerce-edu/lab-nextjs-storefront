3d27a027dcce11dd3212e09bca830ce08d4b8739
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-10-25 11:02:39 -0500

    Implement redirect to checkout

--- lib/bc-client/mutations/createCartRedirect.ts
+++ lib/bc-client/mutations/createCartRedirect.ts
@@ -1,9 +1,55 @@
 import { bcGqlFetch } from "../bc-client-gql";
 
+const createCartRedirectQuery = `
+mutation GetCartRedirectUrls(
+  $cartId: String!
+) {
+  cart {
+    createCartRedirectUrls(
+      input: {
+        cartEntityId: $cartId
+      }
+    ) {
+      redirectUrls {
+        redirectedCheckoutUrl
+      }
+    }
+  }
+}
+`;
+
+type CreateCartRedirectVars = {
+  cartId: string,
+}
+
+type CreateCartRedirectResp = {
+  data: {
+    cart: {
+      createCartRedirectUrls: {
+        redirectUrls: {
+          redirectedCheckoutUrl: string,
+        },
+      },
+    },
+  },
+}
+
 export const createCartRedirect: (
   cartId: string
 ) => Promise<string> = async (
   cartId
 ) => {
-  return '';
+  const cartRedirectResp = await bcGqlFetch<CreateCartRedirectResp, CreateCartRedirectVars>(
+    createCartRedirectQuery,
+    {
+      cartId,
+    }
+  );
+
+  const url = cartRedirectResp.data.cart.createCartRedirectUrls.redirectUrls.redirectedCheckoutUrl;
+  if (!url) {
+    throw new Error("Creating cart redirect URL failed");
+  }
+
+  return url;
 }
