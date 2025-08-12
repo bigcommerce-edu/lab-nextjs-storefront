9bf2a8a976f247c87b89569226f942514a9d1994
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-08-12 15:30:57 -0500

    Add to Cart button on product page

--- components/product/_actions/add-product-to-cart.ts
+++ components/product/_actions/add-product-to-cart.ts
@@ -9,9 +9,11 @@ import { cookies } from "next/headers";
  * Add item to new or existing cart
  */
 export const addProductToCart = async ({
-
+  productId,
+  quantity=1,
 }: {
-
+  productId: number,
+  quantity?: number,
 }) => {
   return Promise.resolve({});
 };
--- components/product/add-to-cart.tsx
+++ components/product/add-to-cart.tsx
@@ -5,13 +5,35 @@ import { useState } from "react";
 import { addProductToCart } from "./_actions/add-product-to-cart";
 
 const AddToCart = ({
-
+  product,
 }: {
-
+  product: Product,
 }) => {
+  const [loading, setLoading] = useState(false);
+
+  const onClick = async () => {
+    setLoading(true);
+
+    const res = await addProductToCart({
+      productId: product.entityId,
+    });
+
+    setLoading(false);
+  }
+
   return (
     <div>
-
+      <button disabled={loading} 
+        className="p-2 rounded-md text-lg w-44 cursor-pointer bg-neutral-700 text-white hover:bg-neutral-500 disabled:bg-neutral-500"
+        onClick={onClick}>
+          {loading ? (
+            <span>...</span>
+          ) : (
+            <span>
+              Add to Cart
+            </span>
+          )}
+        </button>
     </div>
   );
 };
