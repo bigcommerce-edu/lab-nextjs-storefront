9738e7e2fc7b74f3cca2d79af856b2b5b266b6fa
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-08-14 09:14:50 -0500

    Implement Add to Cart logic

--- components/product/_actions/add-product-to-cart.ts
+++ components/product/_actions/add-product-to-cart.ts
@@ -5,6 +5,150 @@ import { getCookieName, isSecure } from "@/lib/cookies";
 import { BasicCart, CartFragment } from "@/types/cart";
 import { cookies } from "next/headers";
 
+const createCartQuery = `
+mutation CreateCart(
+  $productId: Int!,
+  $quantity: Int!
+) {
+  cart {
+    createCart(
+      input: {
+        lineItems: [
+          {
+            quantity: $quantity,
+            productEntityId: $productId
+          }
+        ]
+      }
+    ) {
+      cart {
+        ...cartFields
+      }
+    }
+  }
+}
+
+${CartFragment}
+`;
+
+interface CreateCartVars {
+  productId: number;
+  quantity: number;
+}
+
+interface CreateCartResp {
+  data: {
+    cart: {
+      createCart: {
+        cart: BasicCart & {
+          lineItems: {
+            totalQuantity: number;
+          }
+        }
+      }
+    }
+  }
+}
+
+const createCart = async ({
+  productId,
+  quantity,
+}: {
+  productId: number,
+  quantity: number,
+}) => {
+  const cartResp = await bcGqlFetch<CreateCartResp, CreateCartVars>(
+    createCartQuery,
+    {
+      productId,
+      quantity,
+    }
+  );
+
+  const cart = cartResp.data.cart.createCart.cart;
+
+  return {
+    ...cart,
+    totalQuantity: cart.lineItems.totalQuantity,
+  };
+};
+
+const addCartLineItemQuery = `
+mutation AddCartLineItem(
+  $cartId: String!,
+  $productId: Int!,
+  $quantity: Int!
+) {
+  cart {
+    addCartLineItems(
+      input: {
+        cartEntityId: $cartId,
+        data: {
+          lineItems: [
+            {
+              quantity: $quantity,
+              productEntityId: $productId
+            }
+          ]
+        }
+      }
+    ) {
+      cart {
+        ...cartFields
+      }
+    }
+  }
+}
+
+${CartFragment}
+`;
+
+interface AddCartLineItemVars {
+  cartId: string;
+  productId: number;
+  quantity: number;
+}
+
+interface AddCartLineItemResp {
+  data: {
+    cart: {
+      addCartLineItems: {
+        cart: BasicCart & {
+          lineItems: {
+            totalQuantity: number;
+          }
+        }
+      }
+    }
+  }
+}
+
+const addCartLineItem = async ({
+  cartId,
+  productId,
+  quantity
+}: {
+  cartId: string,
+  productId: number,
+  quantity: number,
+}) => {
+  const cartResp = await bcGqlFetch<AddCartLineItemResp, AddCartLineItemVars>(
+    addCartLineItemQuery,
+    {
+      cartId,
+      productId,
+      quantity,
+    }
+  );
+
+  const cart = cartResp.data.cart.addCartLineItems.cart;
+
+  return {
+    ...cart,
+    totalQuantity: cart.lineItems.totalQuantity,
+  };
+}
+
 /**
  * Add item to new or existing cart
  */
@@ -15,5 +159,37 @@ export const addProductToCart = async ({
   productId: number,
   quantity?: number,
 }) => {
-  return Promise.resolve({});
+  const cookieStore = await cookies();
+  const secure = await isSecure();
+  const cookieName = getCookieName({ name: "cartId", secure });
+
+  const cartId = cookieStore.get(cookieName)?.value;
+
+  let cart = null;
+  if (cartId) {
+    try {
+      cart = await addCartLineItem({ cartId, productId, quantity });
+    } catch (err) {
+      // Existing cart ID might not have matched a cart
+    }
+  }
+
+  if (!cart) {
+    try {
+      cart = await createCart({ productId, quantity });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  if (cart) {
+    cookieStore.set({
+      name: cookieName,
+      value: cart.entityId,
+      httpOnly: true,
+      secure,
+    });
+  }
+
+  return cart;
 };
--- types/cart.ts
+++ types/cart.ts
@@ -1,9 +1,22 @@
 export interface BasicCart {
-
+  entityId: string;
+  currencyCode: string;
+  amount: {
+    value: number;
+  };
 }
 
 export const CartFragment = `
-
+fragment cartFields on Cart {
+  entityId
+  currencyCode
+  amount {
+    value
+  }
+  lineItems {
+    totalQuantity
+  }
+}
 `;
 
 export interface CartItem {
