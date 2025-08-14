7ebb83bec1913ff116d76ca75d5e3210f21a9c74
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-08-14 09:25:49 -0500

    Implement logout logic

--- components/account-links/_actions/logout.ts
+++ components/account-links/_actions/logout.ts
@@ -5,9 +5,38 @@ import { getCookieName, isSecure } from "@/lib/cookies";
 import { getCurrentCustomer } from "@/lib/getCurrentCustomer";
 import { cookies } from "next/headers";
 
+const logoutQuery = `
+mutation Logout {
+  logout {
+    result
+  }
+}
+`;
+
+interface LogoutResp {
+  data: {
+    logout: {
+      result: string;
+    }
+  }
+}
+
 /**
  * Perform logout
  */
 export const logout = async () => {
-  
+  const currentCustomer = await getCurrentCustomer();
+
+  if (currentCustomer?.token) {
+    await bcGqlFetch<LogoutResp>(
+      logoutQuery,
+      currentCustomer?.token
+    );
+  }
+
+  const cookieStore = await cookies();
+  const secure = await isSecure();
+  const cookieName = getCookieName({ name: "customer", secure });
+
+  cookieStore.delete(cookieName);
 };
--- components/account-links/logout-button.tsx
+++ components/account-links/logout-button.tsx
@@ -6,10 +6,17 @@ import { logout } from "./_actions/logout";
 const LogoutButton = () => {
   const [loading, setLoading] = useState(false);
 
+  const submitLogout = async () => {
+    setLoading(true);
+    await logout();
+    setLoading(false);
+  }
+
   return (
     <button 
       disabled={loading} 
       className="mx-4 font-bold cursor-pointer hover:underline"
+      onClick={submitLogout}
     >
       {loading ? (
         <span>...</span>
