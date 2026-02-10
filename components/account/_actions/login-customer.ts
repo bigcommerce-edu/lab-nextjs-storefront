abfda9c3d59f5d389863640401d2a20fde6f18b4
Chris Nanninga <chris.nanninga@bigcommerce.com>
2026-02-10 14:33:35 -0600

    Implement login logic

--- components/account/_actions/login-customer.ts
+++ components/account/_actions/login-customer.ts
@@ -6,17 +6,42 @@ import * as jwt from 'jsonwebtoken';
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-// TODO: Add `loginQuery`
-//  - This is a GraphQL mutation
-//  - It should use login
-//  - Needs variables for $email, $password
-//  - Select entityId from customer, and customerAccessToken.value
+const loginQuery = `
+mutation Login(
+  $email: String!,
+  $password: String!
+) {
+  login(
+    email: $email,
+    password: $password
+  ) {
+    customer {
+      entityId
+    }
+    customerAccessToken {
+        value
+    }
+  }
+}
+`;
 
-// TODO: Define the `LoginVars` interface
-//  - This should match the expected variables for `loginQuery`
+interface LoginVars {
+  email: string;
+  password: string;
+}
 
-// TODO: Define the `LoginResp` interface
-//  - Matches the shape of `loginQuery`
+interface LoginResp {
+  data: {
+    login: {
+      customer: {
+        entityId: number;
+      }
+      customerAccessToken: {
+        value: string;
+      }
+    }
+  }
+}
 
 /**
  * Perform login
@@ -28,18 +53,47 @@ export const loginCustomer = async ({
   email: string,
   password: string,
 }) => {
-  // TODO: Replace this with the actual mutation logic
-  //  - Get JWT_SECRET from environment variables and return an error response if it's not set
-  //  - Use bcGqlFetch with the response and var types
-  //    - Pass loginQuery as the query
-  //    - Pass email, password
-  //  - Extract the customer object from the response
-  //  - Extract the token from the response
-  //    - Return an error response if the token is not found
-  //    - Sign a JWT with the customer entityId and the token
-  //    - Set the customer cookie with the signed JWT
-  //  - If no errors occur, redirect to "/"
-  return Promise.resolve(
-    { success: false, error: 'Login not implemented' }
-  );
+  const { JWT_SECRET } = process.env;
+  if (!JWT_SECRET) {
+    return { success: false, error: "Could not perform login" };
+  }
+
+  try {
+    const customerResp = await bcGqlFetch<LoginResp, LoginVars>(
+      loginQuery,
+      {
+        email,
+        password,
+      }
+    );
+
+    const customer = customerResp.data.login.customer;
+    const token = customerResp.data.login.customerAccessToken.value;
+    if (!customer || !token) {
+      return { success: false, error: "Customer login failed"};
+    }
+
+    const customerJwt = jwt.sign({
+      sub: JSON.stringify({
+        entityId: customer.entityId,
+        token,
+      }),
+    }, JWT_SECRET);
+
+    const cookieStore = await cookies();
+    const secure = await isSecure();
+    const cookieName = getCookieName({ name: "customer", secure });
+
+    cookieStore.set({
+      name: cookieName,
+      value: customerJwt,
+      httpOnly: true,
+      secure,
+    });
+  } catch(err) {
+    const error = (err instanceof Error) ? err.message : String(err);
+    return { success: false, error };
+  }
+
+  redirect("/");
 };
