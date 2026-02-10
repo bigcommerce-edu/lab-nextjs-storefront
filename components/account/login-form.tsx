3b5f850c4c6e075bbeaf54a4fa7ef2ae9c1898e6
Chris Nanninga <chris.nanninga@bigcommerce.com>
2026-02-10 14:31:09 -0600

    Implement login page

--- components/account/login-form.tsx
+++ components/account/login-form.tsx
@@ -4,23 +4,60 @@ import { useState } from "react";
 import { loginCustomer } from "./_actions/login-customer";
 
 const LoginForm = () => {
-  // TODO: Create state values for all form fields
-  //  - email, password
-  // TODO: Create state values for loading and errorMsg
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
-  // TODO: Create `submitLogin` handler
-  //  - Set loading to true and errorMsg to empty before the login action
-  //  - Call loginCustomer with the form fields
-  //  - Set loading to false after the login action completes
-  //  - Check for `success` in the response and set errorMsg accordingly
+  const submitLogin = async () => {
+    setLoading(true);
+    setErrorMsg(null);
+
+    const loginResp = await loginCustomer({
+      email,
+      password,
+    });
+
+    setLoading(false);
+    
+    if (!loginResp.success) {
+      setErrorMsg(loginResp.error ?? "An unexpected error occurred");
+    }
+  }
 
   return (
-    <form>
-      {/* TODO: Render the form */}
-      {/*  - Render errorMsg if it exists */}
-      {/*  - Labels and appropriate inputs for all form fields */}
-      {/*  - Render button with label and disabled state depending on `loading` */}
-      {/*  - Use submitLogin as the onSubmit handler */}
+    <form onSubmit={(e) => {e.preventDefault(); submitLogin()}}
+      className="">
+      {errorMsg && (
+        <div className="bg-rose-400 p-2 font-bold text-sm text-center">
+          {errorMsg}
+        </div>
+      )}
+
+      <div className="my-8 p-4">
+        <label className="font-bold text-lg block">Email</label>
+        <input className="w-full border border-neutral-300 p-2
+          hover:border-neutral-700"
+          type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div className="my-8 p-4">
+        <label className="font-bold text-lg block">Password</label>
+        <input className="w-full border border-neutral-300 p-2
+          hover:border-neutral-700"
+          type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <div className="my-8 p-4 text-center">
+      <button disabled={loading} className="p-2 rounded-md text-lg w-44 cursor-pointer
+        bg-neutral-700 text-white hover:bg-neutral-500 disabled:bg-neutral-500">
+        {loading ? (
+          <span>...</span>
+        ) : (
+          <span>
+            Log In
+          </span>
+        )}
+      </button>
+      </div>
     </form>
   );
 };
