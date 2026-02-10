3548e0471b3439845b11fa9c1eb391225e472f34
Chris Nanninga <chris.nanninga@bigcommerce.com>
2026-02-10 14:38:00 -0600

    Add account links in header

--- lib/getCurrentCustomer.ts
+++ lib/getCurrentCustomer.ts
@@ -3,11 +3,29 @@ import { getCookieName, isSecure } from "./cookies";
 import { cookies } from 'next/headers';
 
 export const getCurrentCustomer = async () => {
-  // TODO: Replace this with the actual logic to get the current customer from the session cookie
-  //  - Use cookies() to get the customer cookie
-  //  - Get JWT_SECRET from environment variables
-  //  - Decode/validate the cookie JWT value
-  //  - Return a result with entityId and token from the decoded payload
-  //  - For any failure, return null
-  return Promise.resolve({});
+  const cookieStore = await cookies();
+  const secure = await isSecure();
+  const cookieName = getCookieName({ name: "customer", secure });
+
+  const customerJwt = cookieStore.get(cookieName)?.value;
+  if (!customerJwt) {
+    return null;
+  }
+
+  const JWT_SECRET = process.env.JWT_SECRET ?? '';
+
+  let customerResult;
+  try {
+    const customerClaim = jwt.verify(customerJwt, JWT_SECRET);
+    const customer = JSON.parse(customerClaim.sub?.toString() ?? '');
+    customerResult = {
+      entityId: parseInt(customer.entityId ?? ''),
+      token: customer.token ?? '',
+    }
+  } catch (err) {
+    customerResult = null;
+    cookieStore.delete(cookieName);
+  }
+
+  return customerResult;
 };
