7fa0ed9b81f08cf8e96e94bfc6786d828d682a8a
Chris Nanninga <chris.nanninga@bigcommerce.com>
2026-02-10 14:23:25 -0600

    Implement register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,9 +4,10 @@ import PageHeading from "@/components/page-heading";
 export default async function RegisterPage() {  
   return (
     <>
-      {/* TODO: Render the register page */}
-      {/*  - Render a PageHeading with "Register" */}
-      {/*  - Render a RegisterForm */}
+      <PageHeading>Register</PageHeading>
+      <div className="w-1/3">
+        <RegisterForm />
+      </div>
     </>
   );
 }
