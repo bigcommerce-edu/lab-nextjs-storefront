59ef2524b25e04a59a814fd3d3cdd0fad2e05b20
Chris Nanninga <chris.nanninga@bigcommerce.com>
2026-02-10 14:39:54 -0600

    Implement logout logic

--- components/account-links/logout-button.tsx
+++ components/account-links/logout-button.tsx
@@ -6,16 +6,17 @@ import { logout } from "./_actions/logout";
 const LogoutButton = () => {
   const [loading, setLoading] = useState(false);
 
-  // TODO: Create `submitLogout` handler
-  //  - Set loading to true before the logout action
-  //  - Call logout action
-  //  - Set loading to false after the logout action completes
+  const submitLogout = async () => {
+    setLoading(true);
+    await logout();
+    setLoading(false);
+  }
 
-  // TODO: Add onClick handler to the button, calling submitLogout
   return (
     <button 
       disabled={loading} 
       className="mx-4 font-bold cursor-pointer hover:underline"
+      onClick={submitLogout}
     >
       {loading ? (
         <span>...</span>
