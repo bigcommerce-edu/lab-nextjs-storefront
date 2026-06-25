132f0bd68c09a1c30ceabd23372bc9799eff6798
Chris Nanninga <tarsusc81@gmail.com>
2026-06-25 17:26:41 -0500

    TODO: Implement category query

--- app/category/[...catPath]/page.tsx
+++ app/category/[...catPath]/page.tsx
@@ -9,13 +9,23 @@ import ArrowLongRight from "@/components/icons/arrow-long-right";
 import { getCurrentCustomer } from "@/lib/getCurrentCustomer";
 
 export default async function CategoryPage({
-
+  // TODO: Add params
 }: {
-
+  // TODO: The type of `params` is a Promise that returns `catPath` (the URL segments)
 }) {
+  // TODO: Get `catPath` from params and form a full path from all segments
+
+  // TODO: Define static mainImgSize and thumbnailSize values
+
+  // TODO: Fetch the category
+  //  - Use `getCategoryWithProducts` 
+  //  - Pass in path, mainImgSize, thumbnailSize, and page
+  //  - For page, pass a hard-coded limit of 12
+
+  // TODO: Log the category response
+
   return (
     <>
-
     </>
   );
 }
