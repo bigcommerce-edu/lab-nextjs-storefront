84cee1e3b297bad7e8e0d95145436bf4d4f46169
Chris Nanninga <tarsusc81@gmail.com>
2026-06-26 09:19:18 -0500

    TODO: Implement register page

--- components/account/_actions/register-customer.ts
+++ components/account/_actions/register-customer.ts
@@ -7,9 +7,11 @@ import { redirect } from "next/navigation";
  * Register a new customer
  */
 export const registerCustomer = async ({
-
+  // TODO: Add destructured parameters
+  //  - email, firstName, lastName, password
 }: {
-
+  // TODO: Add type information for destructured parameters
 }) => {
+  // TODO: Replace this with a response object indicating `success: false` with a "not implemented" error message
   return Promise.resolve({});
 };
