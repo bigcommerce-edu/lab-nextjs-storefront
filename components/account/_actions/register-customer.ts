b9638d08bde5672dd8c53417c8a249181324bef7
Chris Nanninga <tarsusc81@gmail.com>
2026-06-26 09:23:29 -0500

    TODO: Implement customer registration logic

--- components/account/_actions/register-customer.ts
+++ components/account/_actions/register-customer.ts
@@ -3,6 +3,19 @@
 import { bcGqlFetch } from "@/lib/bc-client/bc-client-gql";
 import { redirect } from "next/navigation";
 
+// TODO: Add `registerQuery`
+//  - This is a GraphQL mutation
+//  - It should use customer.registerCustomer
+//  - Needs variables for $email, $firstName, $lastName, $password
+//  - Select entityId from customer
+//  - Select any error message on the various error types (EmailAlreadyInUseError, AccountCreationDisabledError, CustomerRegistrationError, ValidationError)
+
+// TODO: Define the `RegisterVars` interface
+//  - This should match the expected variables for `registerCustomerQuery`
+
+// TODO: Define the `RegisterResp` interface
+//  - Matches the shape of `registerCustomerQuery`
+
 /**
  * Register a new customer
  */
@@ -17,6 +30,13 @@ export const registerCustomer = async ({
   lastName: string,
   password: string,
 }) => {
+  // TODO: Replace this with the actual mutation logic
+  //  - Use bcGqlFetch with the response and var types
+  //    - Pass registerQuery as the query
+  //    - Pass email, firstName, lastName, password
+  //  - Extract the customer object from the response
+  //  - Extract errors from the response and return an appropriate failure response object if any errors are present
+  //  - If no errors occur, redirect to "/login"
   return Promise.resolve(
     { success: false, error: "Registration not implemented." }
   );
